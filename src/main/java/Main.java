619a3a544d059b71cd2e05ba80ad990608a26a43
Plushcake <nikkolo.makeawelli@gmail.com>
2022-03-24 15:03:44 +0300

    fixes #2 Order form added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,12 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-   //service.calculate(amount, registered);
+    //service.calculate(amount, registered);
     long bonus = service.calculate(amount, registered);
     System.out.println (bonus);
   }
 }
 
-//mvn spotbugs:check
-//Данная команда запускает анализ и сообщает о неудачной сборке, если обнаруживает какие-либо ошибки из спектра spotbugs.
+//mvn spotbugs:check - Данная команда запускает анализ и сообщает о неудачной сборке, если обнаруживает какие-либо ошибки из спектра spotbugs.
+
 
